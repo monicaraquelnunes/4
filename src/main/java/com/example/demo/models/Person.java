7a54eefd421d8b03d66a8a83ca4e101d9c54f867
Monica Raquel <mraquel625@gmail.com>
2023-07-25 12:46:24 -0300

    Implementação do CRUD SPRING 1 Crud em API REST com Spring Boot, H2, Maven e JPA Hibernate e o Postman

---
+++ src/main/java/com/example/demo/models/Person.java
@@ -0,0 +1,61 @@
+package com.example.demo.models;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+	
+	@GeneratedValue
+	@Id
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "age")
+	private int age;
+	
+	public Person() {
+		}
+	
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	public int getId() {
+		return id;
+		
+	}
+	
+	public String getName() {
+		return name;
+		
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+		
+	}
+	
+	public int getAge() {
+		return age;
+		
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+
+}
